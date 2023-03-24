cd6578f2ea7512cc65b8a8aa9b4a4af2fa3e6109
mpala <mpalacz00@gmail.com>
2023-03-24 15:55:50 +0100

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,57 @@
+﻿namespace MatrixTestProject1
+{
+    internal class Matrix
+    {
+        public static double[,] matrixAddition(double[,] A, double[,] B)
+        {
+            int Am = A.GetLength(0); // no of rows of A
+            int An = A.GetLength(1); // no of columns of A
+            int Bm = B.GetLength(0); // no of rows of B
+            int Bn = B.GetLength(1); // no of columns of B
+            if ((Am == Bm) && (An == Bn))
+            {
+                double[,] result = new double[Am, An];
+                for (int i = 0; i < Am; i++)
+                    for (int j = 0; j < An; j++)
+                        result[i, j] = A[i, j] + B[i, j];
+                return result;
+            }
+            else return null;
+        }
+        public static double[,] matrixMultiplicationByScalar(double[,] A, double k)
+        {
+            int Am = A.GetLength(0); // no of rows of A
+            int An = A.GetLength(1); // no of columns of A
+            double[,] result = new double[Am, An];
+
+            for (int i = 0; i < Am; i++)
+                for (int j = 0; j < An; j++)
+                    result[i, j] = A[i, j] * k;
+
+            return result;
+        }
+        public static double[,] matrixMultiplication(double[,] A, double[,] B)
+        {
+            int Am = A.GetLength(0); // no of rows of A, i - ndex
+            int An = A.GetLength(1); // no of columns of A, j -index
+            int Bm = B.GetLength(0); // no of rows of B, j- index
+            int Bn = B.GetLength(1); // no of columns of B, k - index
+            double temp;
+            if (An == Bm)
+            {
+                double[,] result = new double[Am, Bn];
+                for (int i = 0; i < Am; i++)
+                    for (int k = 0; k < Bn; k++)
+                    {
+                        temp = 0;
+                        for (int j = 0; j < An; j++)
+                            temp += A[i, j] * B[j, k];
+                        result[i, k] = temp;
+                    }
+                return result;
+            }
+            return null;
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MatrixTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MatrixTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7011679e-5523-4153-9ade-bb5603b7642e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
